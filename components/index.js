e28c78f78fa7828e56e2db498279c5d1dbeb8050
Sriram Vaishnav R <sriramvaishnavr@gmail.com>
2024-01-31 20:38:32 +0530

    add details screen

--- components/index.js
+++ components/index.js
@@ -2,11 +2,15 @@ import HomeHeader from './HomeHeader';
 import FocusedStatusBar from './FocusedStatusBar';
 import NFTCard from './NFTCard';
 import { CircleButton, RectButton } from './Button';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
 
 export {
   HomeHeader,
   FocusedStatusBar,
   NFTCard,
   CircleButton,
-  RectButton
+  RectButton,
+  DetailsDesc,
+  DetailsBid
 }
